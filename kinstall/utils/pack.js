48380442baf05b77bdae3bf657838b98c17a2760
Kamil Tunkiewicz <kamiltunkiewicz@currencyfair.com>
2021-01-24 09:12:03 +0100

    poprawki w pakowaniu modulow

--- kinstall/utils/pack.js
+++ kinstall/utils/pack.js
@@ -1,33 +1,44 @@
 var path = require('path');
 var fs = require('fs');
-const { zipFiles } = require('jszip-glob');
-const JSZip = require('jszip');
+var { zipFiles } = require('jszip-glob');
+var JSZip = require('jszip');
 
-var name = process.argv[2];
 var baseDir = path.join(__dirname, '..', '..');
+var modules = require('../../modules.json')
 
-if (!fs.existsSync(path.join(baseDir, name))) {
-  console.error('Folder ' + name + ' nie istnieje w katalogu profilu.');
-  process.exit(0);
-}
-
-zipFiles('**/*.*', {
-  cwd: path.join(baseDir, name),
-  dot: false,
-  nodir: false,
-  nosort: true,
-  zip: new JSZip(),
-  compression: 'DEFLATE',
-  compressionOptions: {
-    level: 6,
-  },
-}).then(function(zip) {
-  zip
-    .generateNodeStream({ type:'nodebuffer', streamFiles:true })
-    .pipe(fs.createWriteStream(path.join(baseDir, 'dist', name + '.zip')))
-    .on('finish', function () {
-        console.log('Spakowano moduł ' + name + '\n')
+fs.readdir(baseDir, function (err, files) {
+  if (err) {
+    console.error("Could not list the directory.", err);
+    process.exit(1);
+  }
+  files.forEach(function (file, index) {
+    fs.stat(path.join(baseDir, file), function (error, stat) {
+      if (error) {
+        console.error("Error stating file.", error);
+        return;
+      }
+      if (stat.isDirectory() && modules[file]) {
+        zipFiles('**/*.*', {
+          cwd: path.join(baseDir, file),
+          dot: false,
+          nodir: false,
+          nosort: true,
+          zip: new JSZip(),
+          compression: 'DEFLATE',
+          compressionOptions: {
+            level: 6,
+          },
+        }).then(function(zip) {
+          zip
+            .generateNodeStream({ type:'nodebuffer', streamFiles:true })
+            .pipe(fs.createWriteStream(path.join(baseDir, 'dist', file + '.zip')))
+            .on('finish', function () {
+                console.log('Spakowano moduł ' + file + '\n')
+            });
+        }).catch(function(err) {
+          console.error(err);
+        });        
+      }
     });
-}).catch(function(err) {
-  console.error(err);
+  });
 });

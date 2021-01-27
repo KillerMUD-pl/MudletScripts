5d55b0a3b567bd77c31ca236e934abfa15061110
Kamil Tunkiewicz <kamiltunkiewicz@currencyfair.com>
2021-01-27 12:27:28 +0100

    bugfixy

--- kinstall/gui.lua
+++ kinstall/gui.lua
@@ -1,12 +1,40 @@
 module("kinstall/gui", package.seeall)
 
 kinstall.gui = kinstall.gui or {}
-kinstall.gui.containers = {}
+kinstall.gui.moduleContainers = kinstall.gui.moduleContainers or {}
 
 function kinstall:guiInit()
   kinstall.gui.mainCointainer = kinstall.gui.mainCointainer or
     Adjustable.Container:new({
-      name = "HelloWorldContainer",
-      titleText = "Container with a command line"
-    })
+      name = "MainContainer",
+      titleText = "",
+      x="-300px",
+      y="0px",
+      width="275px",
+      height="100%",
+      h_policy=Geyser.Fixed,
+      adjLabelstyle="background-color:rgba(80,80,80,0%);",
+    });
+  kinstall.gui.mainCointainer:show()
+  kinstall.gui.mainCointainer:lockContainer('light')
+
+  kinstall.gui.moduleContainers.kmap = {}
+  kinstall.gui.moduleContainers.kmap['wrapper'] = Adjustable.Container:new({
+    name = "kmap",
+    titleText = "Mapa",
+    x = "0px",
+    y = "0px",
+    width = "100%",
+    height = "100px",
+    h_policy = Geyser.Fixed
+  }, kinstall.gui.mainCointainer)
+
+  kinstall.gui.moduleContainers.kmap['mapper'] = Geyser.Mapper:new({
+    name = "mapper",
+    x = 0,
+    y = 0,
+    width = "100%",
+    height = "100%"
+  }, kinstall.gui.moduleContainers.kmap['wrapper'])
+
 end

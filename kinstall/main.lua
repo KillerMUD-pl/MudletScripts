1a47d7f121a95dabf8c09efbe24ce10c8aa0c058
Zeddicus <kamil.tunkiewicz@gmail.com>
2021-09-18 22:59:09 +0200

    dodano +walk auto

--- kinstall/main.lua
+++ kinstall/main.lua
@@ -579,27 +579,3 @@ function kinstall:require(moduleName)
   package.loaded[filename] = nil
   require(filename)
 end
-
--- -- getting shortpath from windows
--- local function getShortPath(filename)
---   local ok, winapi = pcall(require, "winapi")
---   if ok then
---     winapi.set_encoding(winapi.CP_UTF8)
---     local shortpath = winapi.short_path(filename)
---     if shortpath ~= filename then
---       return shortpath
---     end
---     return filename
---   end
---   return filename
--- end
-
--- -- adding a simplistic Lua package.loader to get around UTF-8 paths issue
--- local function loadLuaFile(filename)
---   local file = io.open(filename, "rb")
---   if file then
---     return assert(loadstring(assert(file:read("*a")), getShortPath(filename)))
---   end
---   return "\n\tno file '".. filename
--- end
--- table.insert(package.loaders, loadLuaFile)
